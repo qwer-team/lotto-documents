43f056469fffca0e01d8e1a19c1649c82e7613b4
rasom <rasom@ukr.net>
2013-04-02 18:42:02 +0300

    Service that returns rates for lotto client.

---
+++ Service/RateService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Qwer\LottoDocumentsBundle\Service;
+
+use Qwer\LottoBundle\Entity\Type;
+use Qwer\LottoBundle\Entity\Client;
+
+class RateService
+{
+
+    public function getRate(array $balls, Type $lottoType, Client $client )
+    {
+        throw new \Exception("not implemented yet");
+    }
+
+}
