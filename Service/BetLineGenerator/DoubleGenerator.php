edaf60b7150ad89352771d64bc0b307c4168a78d
rasom <rasom@ukr.net>
2013-04-08 18:11:21 +0300

    new file:   Service/BetLineGenerator/DoubleGenerator.php

---
+++ Service/BetLineGenerator/DoubleGenerator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Qwer\LottoDocumentsBundle\Service\BetLineGenerator;
+
+use Qwer\LottoDocumentsBundle\Service\BetLineGenerator;
+
+class DoubleGenerator implements BetLineGenerator
+{
+
+    public function getBetLines(array $balls)
+    {
+        
+    }
+
+}
