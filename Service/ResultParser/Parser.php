de82550e4d77f4c1aede319b7fcfce70ec5fa2fb
rasom <rasom@ukr.net>
2013-05-27 15:34:01 +0300

    pars

---
+++ Service/ResultParser/Parser.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Qwer\LottoDocumentsBundle\Service\ResultParser;
+
+interface Parser
+{
+    public function setDraw($draw);
+    public function getUrl();
+    public function parse();
+    public function hasResults();
+}
