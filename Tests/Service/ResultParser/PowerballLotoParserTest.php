58580be1963e84023bd5244ece16b2c5fb2586a5
twin <twin@Dell.(none)>
2013-07-05 15:07:02 +0300

    _

---
+++ Tests/Service/ResultParser/PowerballLotoParserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Qwer\LottoDocumentsBundle\Service\ResultParser\PowerballLotoParser;
+use Qwer\LottoBundle\Entity\Draw;
+use Qwer\LottoBundle\Entity\Result;
+use Symfony\Component\DomCrawler\Crawler;
+
+class PowerballLotoParserTest extends \PHPUnit_Framework_TestCase {
+    
+    public function testParser() {
+        
+        $file = file_get_contents(__DIR__.'/powerball.asp');
+        $crawler = new Crawler($file);
+        $parser = new PowerballLotoParser();
+        $draw = new Draw();
+        $result = new Result();
+        $draw->setResult($result);
+        $date = new \DateTime("2013-07-02");
+        $draw->setDate($date);
+        $parser->setCrawler($crawler);
+        $parser->setDraw($draw);
+        $parser->parse();
+        
+        $this->assertTrue($parser->hasResults());
+        $this->assertEquals($result->getAllBalls(), array(13, 18, 20, 7, 12, 1));
+    }
+}
+?>
