aea58c8c27b3ec51dce9ce334989dbf3db4bfbe4
twin <twin@Dell.(none)>
2013-07-30 19:19:51 +0300

    _

--- Tests/Service/ResultParser/Ireland/IrishLotoParserTest.php
+++ Tests/Service/ResultParser/Ireland/IrishLotoParserTest.php
@@ -1,27 +1,27 @@
 <?php
 
-use Qwer\LottoDocumentsBundle\Service\ResultParser\Ireland\Plus1LotoParser;
+use Qwer\LottoDocumentsBundle\Service\ResultParser\Ireland\IrishLotoParser;
 use Qwer\LottoBundle\Entity\Draw;
 use Qwer\LottoBundle\Entity\Result;
 use Symfony\Component\DomCrawler\Crawler;
 
-class Plus1LotoParserTest extends \PHPUnit_Framework_TestCase {
-    
-    public function testParser() {
-        
-        $file = file_get_contents(__DIR__.'/plus1.htm');
+class IrishLotoParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function testParser()
+    {
+        $file = file_get_contents(__DIR__.'/results-2013.html');
         $crawler = new Crawler($file);
-        $parser = new Plus1LotoParser();
+        $parser = new IrishLotoParser();
         $draw = new Draw();
         $result = new Result();
         $draw->setResult($result);
-        $date = new \DateTime("2013-07-13");
+        $date = new \DateTime("2013-07-27");
         $draw->setDate($date);
         $parser->setCrawler($crawler);
         $parser->setDraw($draw);
         $parser->parse();
         
         $this->assertTrue($parser->hasResults());
-        $this->assertEquals($result->getAllBalls(), array(6, 11, 15, 23, 27, 44, 40));
+        $this->assertEquals($result->getAllBalls(), array(9, 22, 37, 40, 42, 44, 25));
     }
 }
